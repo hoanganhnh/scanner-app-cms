33e81ab36415c2b26215e9f82417d21c537921be
hoanganh <hoahoanganh20012001@gmail.com>
2022-12-05 23:41:49 +0700

    feat: update product favorite

--- src/api/favorites-product/controllers/favorites-product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * favorites-product controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::favorites-product.favorites-product');
--- src/api/favorites-product/services/favorites-product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * favorites-product service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::favorites-product.favorites-product');
